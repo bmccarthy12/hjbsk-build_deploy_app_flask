743c80b4a684a3d4e9f0c2f6f6667bf5f0ddfa7d
bmccarthy12 <bmccarthy@potomacschool.org>
2024-04-16 14:04:34 -0400

    finishing lab

---
+++ maths/mathematics.py
@@ -0,0 +1,11 @@
+def summation(a,b):
+    result = a+b
+    return result
+
+def subtraction(a,b):
+    result = a-b
+    return result
+
+def multiply(a,b):
+    result = a*b
+    return result
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+from maths.mathematics import summation, multiply, subtraction
 # Import the Maths package here
 
 app = Flask("Mathematics Problem Solver")
@@ -8,22 +9,30 @@ def sum_route():
     num1 = float(request.args.get('num1'))
     num2 = float(request.args.get('num2'))
     # Write your code here
+    result = summation(num1,num2)
+    return str(result)
 
 @app.route("/sub")
 def sub_route():
     num1 = float(request.args.get('num1'))
     num2 = float(request.args.get('num2'))
     # Write your code here
+    result = subtraction(num1,num2)
+    return str(result)
 
 @app.route("/mul")
 def mul_route():
     num1 = float(request.args.get('num1'))
     num2 = float(request.args.get('num2'))
     # Write your code here  
+    result = multiply(num1,num2)
+    return str(result)
 
 @app.route("/")
 def render_index_page():
     # Write your code here
+    return render_template('index.html')
+
     
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
